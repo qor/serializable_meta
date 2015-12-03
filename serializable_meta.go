d30f0f789652175a72b916b8e90756e6e820ab87
Jinzhu <wosmvp@gmail.com>
2015-12-03 14:32:27 +0800

    Rename SerializeArgument to SerializableArgument

--- serializable_meta.go
+++ serializable_meta.go
@@ -9,23 +9,23 @@ import (
 )
 
 type SerializableMetaInterface interface {
-	GetSerializeArgumentResource() *admin.Resource
-	GetSerializeArgument(SerializableMetaInterface) interface{}
-	GetSerializeArgumentKind() string
-	SetSerializeArgumentValue(interface{})
+	GetSerializableArgumentResource() *admin.Resource
+	GetSerializableArgument(SerializableMetaInterface) interface{}
+	GetSerializableArgumentKind() string
+	SetSerializableArgumentValue(interface{})
 }
 
-type SerializeArgument struct {
+type SerializableArgument struct {
 	Kind  string
 	Value string `sql:"size:65532"`
 }
 
-func (serialize SerializeArgument) GetSerializeArgumentKind() string {
+func (serialize SerializableArgument) GetSerializableArgumentKind() string {
 	return serialize.Kind
 }
 
-func (serialize *SerializeArgument) GetSerializeArgument(serializableMetaInterface SerializableMetaInterface) interface{} {
-	if res := serializableMetaInterface.GetSerializeArgumentResource(); res != nil {
+func (serialize *SerializableArgument) GetSerializableArgument(serializableMetaInterface SerializableMetaInterface) interface{} {
+	if res := serializableMetaInterface.GetSerializableArgumentResource(); res != nil {
 		value := res.NewStruct()
 		json.Unmarshal([]byte(serialize.Value), value)
 		return value
@@ -33,13 +33,13 @@ func (serialize *SerializeArgument) GetSerializeArgument(serializableMetaInterfa
 	return nil
 }
 
-func (serialize *SerializeArgument) SetSerializeArgumentValue(value interface{}) {
+func (serialize *SerializableArgument) SetSerializableArgumentValue(value interface{}) {
 	if bytes, err := json.Marshal(value); err == nil {
 		serialize.Value = string(bytes)
 	}
 }
 
-func (serialize *SerializeArgument) ConfigureQorResourceBeforeInitialize(res resource.Resourcer) {
+func (serialize *SerializableArgument) ConfigureQorResourceBeforeInitialize(res resource.Resourcer) {
 	if res, ok := res.(*admin.Resource); ok {
 		if _, ok := res.Value.(SerializableMetaInterface); ok {
 			if res.GetMeta("Kind") == nil {
@@ -50,15 +50,15 @@ func (serialize *SerializeArgument) ConfigureQorResourceBeforeInitialize(res res
 						if context.GetDB().NewScope(value).PrimaryKeyZero() {
 							return nil
 						} else {
-							return value.(SerializableMetaInterface).GetSerializeArgumentKind()
+							return value.(SerializableMetaInterface).GetSerializableArgumentKind()
 						}
 					},
 				})
 			}
 
-			if res.GetMeta("SerializeArgument") == nil {
+			if res.GetMeta("SerializableArgument") == nil {
 				res.Meta(&admin.Meta{
-					Name: "SerializeArgument",
+					Name: "SerializableArgument",
 					Type: "serialize_argument",
 					Valuer: func(value interface{}, context *qor.Context) interface{} {
 						if serializeArgument, ok := value.(SerializableMetaInterface); ok {
@@ -66,15 +66,15 @@ func (serialize *SerializeArgument) ConfigureQorResourceBeforeInitialize(res res
 								Value    interface{}
 								Resource *admin.Resource
 							}{
-								Value:    serializeArgument.GetSerializeArgument(serializeArgument),
-								Resource: serializeArgument.GetSerializeArgumentResource(),
+								Value:    serializeArgument.GetSerializableArgument(serializeArgument),
+								Resource: serializeArgument.GetSerializableArgumentResource(),
 							}
 						}
 						return nil
 					},
 					Setter: func(result interface{}, metaValue *resource.MetaValue, context *qor.Context) {
 						if serializeArgument, ok := result.(SerializableMetaInterface); ok {
-							serializeArgumentResource := serializeArgument.GetSerializeArgumentResource()
+							serializeArgumentResource := serializeArgument.GetSerializableArgumentResource()
 							value := serializeArgumentResource.NewStruct()
 
 							for _, meta := range serializeArgumentResource.GetMetas([]string{}) {
@@ -85,14 +85,14 @@ func (serialize *SerializeArgument) ConfigureQorResourceBeforeInitialize(res res
 								}
 							}
 
-							serializeArgument.SetSerializeArgumentValue(value)
+							serializeArgument.SetSerializableArgumentValue(value)
 						}
 					},
 				})
 			}
 
-			res.NewAttrs("Kind", "SerializeArgument")
-			res.EditAttrs("ID", "Kind", "SerializeArgument")
+			res.NewAttrs("Kind", "SerializableArgument")
+			res.EditAttrs("ID", "Kind", "SerializableArgument")
 		}
 	}
 }
